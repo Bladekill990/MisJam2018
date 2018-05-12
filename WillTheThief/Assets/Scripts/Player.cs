1e313906aaae86ec9930cb9c8c49c7f32d86e14e
Justin Ste. Marie <justinstemarie@gmail.com>
2018-05-12 02:45:13 -0400

    PlayerMoves
    Camera follows player (temp)

---
+++ WillTheThief/Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+
+    void FixedUpdate()
+    {
+        movement();
+    }
+
+
+    void movement()
+    {
+        float speed = 5.0f;
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+        GetComponent<Rigidbody>().position += z * transform.forward * Time.deltaTime * speed;
+        GetComponent<Rigidbody>().position += x * transform.right * Time.deltaTime * speed;
+    }
+
+
+}
