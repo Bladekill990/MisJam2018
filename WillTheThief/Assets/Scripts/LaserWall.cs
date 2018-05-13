da9aa1bdaac1d0a1c0205687254af146b4397a09
Justin Ste. Marie <justinstemarie@gmail.com>
2018-05-12 20:18:42 -0400

    AllTheWallsButNot

---
+++ WillTheThief/Assets/Scripts/LaserWall.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserWall : MonoBehaviour {
+
+    public OnoffSwitch swch;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
